a1f9793157febd1595a8753f6e1adae5f794530c
Mario Valle Reyes <120954554+mariovallereyes@users.noreply.github.com>
2024-08-16 12:04:10 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,31 +3,33 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 
-# Leer las credenciales del archivo config.json
+# Cargar credenciales desde config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-# Configuración de la API de Airtable (se extrae del archivo config.json)
-API_KEY = config["AIRTABLE_API_KEY"]
-BASE_ID = config["BASE_ID"]
-TABLE_ID = config["TABLE_ID"]
+# Configuración de Airtable
+API_KEY = config['AIRTABLE_API_KEY']
+BASE_ID = config['AIRTABLE_BASE_ID']
+TABLE_ID = config['AIRTABLE_TABLE_ID']
 
-# Configuración de Google Sheets y Google Drive (se extrae del archivo config.json)
-SHEET_ID = config["SHEET_ID"]
+# Configuración de Google Sheets y Drive
+SHEET_ID = config['GOOGLE_SHEET_ID']
+CREDENTIALS_FILE = config['GOOGLE_CREDENTIALS_FILE']
+
+# Alcance (scope) de Google Sheets y Drive
 SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-CREDENTIALS_FILE = config["CREDENTIALS_FILE"]
 
-# Autenticar y acceder a Google Sheets y Drive
+# Autenticación y acceso a Google Sheets y Drive
 creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, SCOPE)
 client = gspread.authorize(creds)
 
-# Acceder a la hoja de cálculo de Google Sheets
+# Acceder a Google Sheet
 sheet = client.open_by_key(SHEET_ID).sheet1
 
-# Obtener todas las filas de la hoja
+# Obtener todas las filas de datos desde la hoja
 rows = sheet.get_all_values()
 
-# Bucle a través de las filas comenzando desde la segunda fila (asumiendo que la primera fila son encabezados)
+# Recorrer las filas comenzando desde la segunda fila (suponiendo que la primera es encabezado)
 for i, row in enumerate(rows[1:], start=2):  # enumerate comienza en 2 para coincidir con los números de fila en Google Sheets
     date_time, gmail_account, subject, tweet_url, tweet_text, attachment_url, processed = row
     
@@ -39,7 +41,7 @@ for i, row in enumerate(rows[1:], start=2):  # enumerate comienza en 2 para coin
     handle = tweet_url.split('/')[3]
     name = handle
     
-    # Convertir el URL de Google Drive a un URL de descarga directa
+    # Convertir el URL de Google Drive a un URL de descarga
     file_id = attachment_url.split("id=")[-1]
     direct_url = f"https://drive.google.com/uc?export=download&id={file_id}"
     
@@ -56,7 +58,6 @@ for i, row in enumerate(rows[1:], start=2):  # enumerate comienza en 2 para coin
             "Handle": f"https://x.com/{handle}",
             "Tweet Text": tweet_text,
             "URL": tweet_url,
-            # Usamos el enlace directo de Google Drive
             "Attachment": [{"url": direct_url}]
         }
     }
@@ -64,7 +65,7 @@ for i, row in enumerate(rows[1:], start=2):  # enumerate comienza en 2 para coin
     # Endpoint de la API de Airtable
     endpoint_url = f"https://api.airtable.com/v0/{BASE_ID}/{TABLE_ID}"
     
-    # Encabezados HTTP para la solicitud
+    # Headers HTTP para el request
     headers = {
         "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json"
@@ -73,7 +74,7 @@ for i, row in enumerate(rows[1:], start=2):  # enumerate comienza en 2 para coin
     # Enviar la entrada a Airtable
     response = requests.post(endpoint_url, json=data, headers=headers)
     
-    # Confirmar respuesta y marcar la fila como procesada
+    # Confirmar la respuesta y marcar la fila como procesada
     if response.status_code == 200:
         print("Nueva entrada en Airtable, mi carnal!")
         sheet.update_cell(i, 7, "Yes")  # "Procesado" (Columna G en GSheets)
